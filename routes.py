84f5780b0ad885b43a636226a44d926501480ea3
mateppon <73835864+mateppon@users.noreply.github.com>
2024-10-05 16:41:27 +0300

    Get data from form

--- routes.py
+++ routes.py
@@ -1,4 +1,4 @@
-from flask import redirect, render_template, request, session, jsonify
+from flask import redirect, render_template, request, session
 import json
 from app import app
 import users
@@ -9,9 +9,27 @@ def index():
     userlist = users.all_users()
     return render_template("index.html", count=len(userlist), users=userlist)
 
-@app.route("/map")
+@app.route("/newrestaurant", methods = ["GET", "POST"])
 def map():
-    return render_template("map.html")
+    if request.method == "GET":
+        return render_template("newrestaurant.html")
+
+    if request.method == "POST":
+            if request.is_json:
+                data = request.json
+                if 'center' in data:
+                  center = data['center']
+                  print(f"{center}")
+            else:
+                name = request.form["name"]
+                description= request.form["description"]
+                categories = request.form.getlist("categories")
+                print(name, description)
+                print("check" in request.form)
+                for category in categories:
+                    print(category)
+
+            return redirect("/newrestaurant")
 
 
 @app.route("/login", methods=["POST"])
--- static/leaflet.js
+++ static/leaflet.js
@@ -4,12 +4,20 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
-
 var _geocoderType = L.Control.Geocoder.nominatim();
+
 var geocoder = L.Control.geocoder({
     geocoder: _geocoderType
 }).addTo(map);
 
-
-
-
+geocoder.on('markgeocode', function (event) {
+    var center = event.geocode.center;
+    L.marker(center).addTo(map);
+    map.setView(center, map.getZoom());
+         $.ajax({
+            url: '/newrestaurant',
+            method: 'POST',
+            data: JSON.stringify({ center: center}),
+            contentType: 'application/json'
+        });
+});
