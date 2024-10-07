db21c2e774c26a242cf26ccc018a5b20e4a0e9f2
mateppon <73835864+mateppon@users.noreply.github.com>
2024-10-07 10:51:58 +0300

    Add restaurant to db

--- restaurants.py
+++ restaurants.py
@@ -5,20 +5,20 @@ from db import db
 
 
 
-def add_restaurant(name :str, description :str, categories : list, address : str):
+def add_restaurant(name :str, info :str, categories : list, address : str):
 
-    creator = session.get("user_id")
-    print(creator)
+    creator_id = session.get("user_id")
     visible = True
 
     try:
-        sql = text("INSERT INTO restaurants (creator_id, name, info, visible) VALUES (:creator, :name, :info, :visible)")
-        db.session.execute(sql, {"creator_id":creator, "name":name, "info":description, "visible": visible})
+        sql = text("INSERT INTO restaurants (creator_id, name, info, visible) VALUES (:creator_id, :name, :info, :visible)")
+        db.session.execute(sql, {"creator_id":creator_id, "name":name, "info":info, "visible": visible})
         db.session.commit()
     except:
             return False
 
     try:
+
         sql = text("SELECT id FROM restaurants WHERE name=:name")
         result = db.session.execute(sql, {"name":name})
         restaurant = result.fetchone()
@@ -26,10 +26,11 @@ def add_restaurant(name :str, description :str, categories : list, address : str
             return False
 
     restaurant_id = restaurant.id
+    coordinates = (address["lat"],address["lng"])
 
     try:
-        sql = text("INSERT INTO addresses (restaurant_id, coordinates) VALUES (:restaurant_id, :address)")
-        db.session.execute(sql, {"restaurant_id":restaurant_id, "coordinates":address})
+        sql = text("INSERT INTO addresses (restaurant_id, coordinates) VALUES (:restaurant_id, :coordinates)")
+        db.session.execute(sql, {"restaurant_id":restaurant_id, "coordinates":coordinates})
         db.session.commit()
     except:
             return False
