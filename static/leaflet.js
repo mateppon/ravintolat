002cd33d92a1370439db579a61b8484fc74e678b
mateppon <73835864+mateppon@users.noreply.github.com>
2024-10-20 21:40:01 +0300

    Edit layout

--- static/leaflet.js
+++ static/leaflet.js
@@ -35,7 +35,6 @@ document.addEventListener('DOMContentLoaded', function() {
             e.preventDefault();
             return false;
         }
-        alert("Ravintola on lisätty. Palaa etusivulle. ")
         return true;
     });
 });
